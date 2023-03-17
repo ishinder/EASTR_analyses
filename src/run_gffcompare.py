fbdd5733caede8a7b9ab02bfb1db2168f7db99d4
ishinder <ida.shinder@gmail.com>
2023-03-17 13:33:26 -0400

    modified run_gffcompare to run in parallel

--- src/run_gffcompare.py
+++ src/run_gffcompare.py
@@ -2,12 +2,12 @@
 import glob
 import shlex
 import subprocess
-from collections import defaultdict
+
 import sys
-from EASTR import utils
 import glob
 import argparse
 import os
+import multiprocessing
 
 def parse_args(arglist):
     parser = argparse.ArgumentParser(
@@ -63,6 +63,12 @@ def run_gffcompare(gtf_path, outdir, ref_gtf, name):
         cmd = f"gffcompare -r {ref_gtf} {gtf_path} -o {name}"
         subprocess.run(shlex.split(cmd), check=True)
 
+def run_gffcompare_parallel(files, outdir, ref_gtf, p=1):
+    pool = multiprocessing.Pool(p)
+    args_list = [(file, outdir, ref_gtf, os.path.basename(file).split('.')[0].replace('.', '_')) for file in files]
+    pool.starmap(run_gffcompare, args_list)
+    pool.close()
+    pool.join()
 
 def print_gffcompare_results(filter_by, outdir, glob_by):
     cmd=f"grep \"{filter_by}\" {outdir}/*/*{glob_by}*stats | awk -F'original/|filtered/' '{{print $2}}' | sort -nk1,1 | column -t"
@@ -92,22 +98,20 @@ def main(arglist=None):
     if filters is None:
         filters="Transcript level, Intron level, Missed exons, Novel introns, Novel exons, Novel loci, Matching transcripts"
 
-    filters = filters.split(', ')
+    if p is None:
+        p = 1
 
-    basedir="/ccb/salz2/shinder/projects/EASTR_tests2/lieber_sra"
-    outdir=f"{basedir}/gffcompare"
-    ref_gtf="/ccb/salz1/mpertea/stringtie/paper/hg38c_protein_and_lncRNA.gtf"
+    filters = filters.split(', ')
 
     gtf_list = {}
     for t in ["original","filtered"]:
         odir = f"{outdir}/{t}"
-        utils.make_dir(odir)
+        make_dir(odir)
         os.chdir(odir)
         gtf_list[t] = glob.glob(f"{gtfpath}/{t}/*{glob_by}*.gtf")
-        for file in gtf_list[t]:
-            name=os.path.basename(file).split('.')[0]
-            if not os.path.exists(f"{odir}/{name}.stats"):
-                run_gffcompare(file, odir, ref_gtf, name)
+
+    gtf_files = gtf_list["original"] + gtf_list["filtered"]
+    run_gffcompare_parallel(gtf_files, outdir, ref_gtf, p=p)
 
     # filters=["Novel loci"]
     for filter_by in filters:
@@ -123,7 +127,9 @@ def test_main():
     arglist=["--dir", "/ccb/salz2/shinder/projects/EASTR_tests2/lieber_sra",
                    "-r", "/ccb/salz1/mpertea/stringtie/paper/hg38c_protein_and_lncRNA.gtf", 
                    "--filters", "Novel loci, Novel exons, Novel introns, Missed exons, Intron level, Transcript level, Matching transcripts", 
-                   "--glob_by", ""]
+                   "--glob_by", "",
+                   "-p", "12"]
+    main(arglist)
 
 
 if __name__ == '__main__':
